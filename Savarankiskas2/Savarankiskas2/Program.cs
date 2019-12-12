88c6e4fac6c8f14d3c07e56abb985db5c884f96f
tofanijo <tofanijo@gmail.com>
2019-12-12 21:58:51 +0200

    Savarankiskas2

---
+++ Savarankiskas2/Savarankiskas2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankiskas2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 1;
+            int B = 15;
+            int C;
+
+            C = A;
+            A = B;
+            B = C;
+
+            Console.WriteLine("A: {0}", A);
+            Console.WriteLine("B: {0}", B);
+            Console.ReadLine();
+
+        }
+    }
+}
