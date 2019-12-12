e042b5a9dd38857c69dc6d8a11f0a3cf399ce779
tofanijo <tofanijo@gmail.com>
2019-12-12 21:28:25 +0200

    savarankiskas1

---
+++ savarankiskas1/savarankiskas1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savarankiskas1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ratas = "";
+            Console.WriteLine("Iveskite skaiciu:");
+            ratas = Console.ReadLine();
+
+            Console.Write("{0} {0} {0}, {0}",  ratas);
+            Console.ReadLine();
+        }
+    }
+}
