a48ce1b1387bfabae74e80bb1b28e5b5c8bad28b
tofanijo <tofanijo@gmail.com>
2019-12-12 19:55:01 +0200

    saugoma informacija

---
+++ saugoma informacija/saugoma informacija/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("saugoma informacija")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("saugoma informacija")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b2592723-6e3f-4795-9a8a-39c70c6d8e34")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ saugoma informacija1/saugoma informacija1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace saugoma_informacija1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime datetoday;
+            string kursopav;
+            string mokyklospav;
+            int studentskaic;
+
+            datetoday = DateTime.Now;
+            kursopav = "NTV6";
+            mokyklospav = "VGTU";
+            studentskaic = 19 ;
+
+            Console.WriteLine("Siandienos data: {0}", datetoday);
+            Console.WriteLine("Kurso pavadinimas: {0}", kursopav);
+            Console.WriteLine("Mokyklos pavadinimas: {0}", mokyklospav);
+            Console.WriteLine("Studentu skaicius: {0}", studentskaic);
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
