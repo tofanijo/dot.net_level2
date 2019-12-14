e05eaa123f78cfa4c79e47edfc7988d45cfc019b
tofanijo <tofanijo@gmail.com>
2019-12-14 20:35:07 +0200

    staciakampio plotas

---
+++ Staciakampio ploto skaiciuokle/Staciakampio ploto skaiciuokle/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staciakampio_ploto_skaiciuokle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Staciakampio ploto skaiciuokle");
+            Console.WriteLine("[Pradeti - Enter] [Iseiti -e]");
+            while (Console.ReadLine() != "e")
+
+            {
+                try
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Iveskite staciakampio ilgi m");
+                    double sk1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Iveskite staciakampio ploti m");
+                    double sk2 = Convert.ToDouble(Console.ReadLine());
+
+
+                    double vidurkis = (sk1 * sk2);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Staciakampio plotas: {0}", vidurkis);
+                    Console.WriteLine();
+                    //Console.WriteLine("[Pakartoti - Enter] [iseiti - e]");
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("Skaiciu vidurkio saiciavimas");
+                    Console.WriteLine("[Pradeti - Enter] [Iseiti - e]");
+                }
+            }
+        }
+    }
+}
+
---
+++ staciakampio plotas/staciakampio plotas/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staciakampio_plotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            double Plotas;
+            string veiksmas = "";
+            while (veiksmas != "exit")
+            {
+                Console.WriteLine("Iveskite staciakampio ilgi:");
+                A = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("iveskite veiksma [+, -, /, *], arba exit");
+                veiksmas = Console.ReadLine();
+                Console.WriteLine("Iveskite staciakampio ploti:");
+                B = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (veiksmas)
+                {
+                    case "*":
+                        Plotas = A * B;
+                        RodytiRezultata(Plotas);
+                        break;
+                    case "exit":
+                        Console.WriteLine("Viso gero!");
+                        Console.ReadLine();
+                        break;
+
+                    default:
+                        Console.WriteLine("Neteisingai ivestas veiksmas, pakartokite");
+                        break;
+                }
+            }
+            void RodytiRezultata(double aRezultatas)
+            {
+                {
+                    Console.WriteLine("Plotas yra : {0}", aRezultatas);
+
+
+
+
+                }
+
+            }
+        }
+    }
+}
