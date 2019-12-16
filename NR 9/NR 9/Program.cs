9de860896041ccb0ca75786c91827e62e503f525
tofanijo <tofanijo@gmail.com>
2019-12-16 20:52:17 +0200

    adresas

---
+++ NR 9/NR 9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string miestas;
+            string gatve;
+            double namoNr;
+            double namoStatybosMetai;
+
+            miestas = "Vilnius";
+            gatve = "Sviesos g";
+            namoNr = 11;
+            namoStatybosMetai = 1987;
+
+            Console.WriteLine("Miesto pavadinimas: {0}", miestas);
+            Console.WriteLine("Gatves pavadinimas: {0}", gatve);
+            Console.WriteLine("Namo numeris: {0}", namoNr);
+            Console.WriteLine("Namo numeris: {0}", namoStatybosMetai);
+            Console.ReadLine();
+        }
+    }
+}
