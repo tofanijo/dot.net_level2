2efed236d8b0e09d10e01812a50d284709fa197b
tofanijo <tofanijo@gmail.com>
2019-12-16 20:32:30 +0200

    Vardas Pavarde

---
+++ NR.8/NR.8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Vardas = "";
+            string Pavarde = "";
+            Console.WriteLine("Iveskite savo Varda:");
+            Vardas= Console.ReadLine();
+            Console.WriteLine("Iveskite savo Pavarde:");
+            Pavarde = Console.ReadLine();
+
+            Console.Write("Jusu Vardas Pavarde: {0} {1}", Vardas, Pavarde);
+            Console.ReadLine();
+        }
+    }
+}
