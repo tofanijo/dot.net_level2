483f4511504d0cbceb270d682c9caf614b66d865
tofanijo <tofanijo@gmail.com>
2019-12-16 14:41:52 +0200

    Tuscia eilute/false.

---
+++ NR.6/NR.6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool tusciaEilute;
+            Console.WriteLine("Iveskite betkokio teksto eilute");
+            string tekstas = Console.ReadLine();
+
+            if(tekstas.Length !=0)
+            {
+                tusciaEilute = true;
+            }
+            else
+            {
+                tusciaEilute = false;
+
+            }
+            Console.WriteLine(tusciaEilute);
+            Console.ReadKey();
+        }
+    }
+}
