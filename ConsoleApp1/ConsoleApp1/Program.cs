0e778835ef4388d606292141f8b8f5a7fdfb4a95
tofanijo <tofanijo@gmail.com>
2019-12-16 15:06:37 +0200

    true>0,false<0

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Iveskite skaiciu Nr.1");
+            double skaicius1 = Convert.ToDouble(Console.ReadLine());
+
+            Palyginimas(skaicius1);
+
+            Console.WriteLine("Skaicius > uz 0:" + Palyginimas(skaicius1));
+            Console.ReadLine();
+
+            bool Palyginimas(double aSkaicius1)
+            {
+                if (aSkaicius1 < 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+
+
+            }
+        }
+    }
+}
+
+
+
