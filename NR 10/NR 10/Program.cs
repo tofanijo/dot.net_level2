e89e7cb4e49225c9a41d31cd83dacfb6de36b70f
tofanijo <tofanijo@gmail.com>
2019-12-17 12:36:16 +0200

    Suvedamas adresas

---
+++ NR 10/NR 10/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite miesto pavadinima");
+
+            string miestoPavadinimas = Console.ReadLine();
+
+            Console.WriteLine("Iveskite gatves pavadinima");
+
+            string gatvesPavadinimas = Console.ReadLine();
+
+            Console.WriteLine("Iveskite namo numeri");
+
+            string namoNumeris = Console.ReadLine();
+
+            Console.WriteLine("Iveskite statymo pradzios data (pvz.: 2018/10/10)");
+
+            DateTime ivestiStatymoPradziosMetai = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Iveskite pastatymo data (pvz.: 2018/10/10)");
+
+            DateTime ivestiPastatymoMetai = DateTime.Parse(Console.ReadLine());
+
+
+
+            RodytiDuomenis(miestoPavadinimas, gatvesPavadinimas, namoNumeris);
+
+
+
+            Console.ReadLine();
+
+
+
+            void RodytiDuomenis(string aMiestoPavadinimas, string aGatvesPavadinimas, string aNamoNumeris)
+
+            {
+
+                Console.WriteLine("Miestas: {0}", aMiestoPavadinimas, aGatvesPavadinimas, aNamoNumeris);
+
+                Console.WriteLine("Gatve: {1}", aMiestoPavadinimas, aGatvesPavadinimas, aNamoNumeris);
+
+                Console.WriteLine("Namo Nr: {2}", aMiestoPavadinimas, aGatvesPavadinimas, aNamoNumeris);
+
+                Console.WriteLine("Statymo pradzios metai: {0}", ivestiStatymoPradziosMetai);
+
+                Console.WriteLine("Pastatymo metai: {0}", ivestiPastatymoMetai);
+
+                Console.WriteLine("Statybos laikotarpis metais: {0:0.0}", (ivestiPastatymoMetai - ivestiStatymoPradziosMetai).TotalDays / 365);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ NR 9.1/NR 9.1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR_9._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string miestas;
+            //string gatve;
+            //string namoNr;
+            //string DateTime;
+
+            //miestas = "";
+            //gatve = "";
+            //namoNr = "";
+            //DateTime = "";
+
+
+            
+            Console.WriteLine("Miesto pavadinimas:");
+            string miestas =  Console.ReadLine();
+            
+            Console.WriteLine("Gatves pavadinimas:");
+            string gatve = Console.ReadLine();
+           
+            Console.WriteLine("Namo numeris:");
+            string namoNr = Console.ReadLine();
+            
+            Console.WriteLine("Namo statybos metai:");
+            DateTime statybosMetai = DateTime.Parse(Console.ReadLine());
+
+            RodytiInfo(miestas, gatve, namoNr);
+
+            Console.ReadLine();
+
+
+
+            void RodytiInfo(string amiestas, string agatve, string anamorNr)
+            {
+                Console.WriteLine("Miestas: {0}", amiestas, agatve, anamorNr);
+                Console.WriteLine("Gatve: {1}", miestas, agatve, anamorNr);
+                Console.WriteLine("Namo nr: {2}", miestas, agatve, anamorNr);
+                Console.WriteLine("Statybos metai: {0}", statybosMetai);
+                Console.ReadLine();
+                
+
+                
+
+
+               
+
+
+
+            }
+        }
+    }
+}
